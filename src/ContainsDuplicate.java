153be24c7d9e31c9e338a4eafeb4d31a6b571358
Lorenzo <mail.lorenzo21@gmail.com>
2026-07-12 12:22:10 +0530

    Contains duplicate

---
+++ src/ContainsDuplicate.java
@@ -0,0 +1,16 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+    public boolean solution(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                return true;
+            } else {
+                set.add(nums[i]);
+            }
+        }
+        return false;
+    }
+}
