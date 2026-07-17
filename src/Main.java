3885370481614bff16fc52f84807b37ee94caa67
Lorenzo <mail.lorenzo21@gmail.com>
2026-07-17 20:02:13 +0530

    Product of array except self

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Solution s = new Solution();
+        s.productExceptSelf(new int[]{1, 2, 3, 4});
     }
 }
