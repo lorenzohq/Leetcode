664303836f5bdbc90e74a4ee659ee1a6725a7c04
Lorenzo <mail.lorenzo21@gmail.com>
2026-07-20 21:50:45 +0530

    Valid anagram added

---
+++ ValidAnagram.ts
@@ -0,0 +1,23 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) {
+        return false
+    }
+
+    let map: Map<string, number> = new Map();
+
+    for (const c of s) {
+        map.set(c, (map.get(c) || 0) + 1)
+    }
+
+    for (const c of t) {
+        const count = map.get(c) ?? 0;
+        if (count == 0) {
+            return false
+        }
+        map.set(c, count - 1);
+    }
+
+    return true;
+};
+
+console.log(isAnagram("car", "rat"))
